5f8adf6c0311cc17b4bae88869c333e2363e260f
Cassidy Waldrip <cassidy.waldrip@gmail.com>
2020-03-19 12:10:40 -0600

    bug

--- src/views/Animals.vue
+++ src/views/Animals.vue
@@ -1,33 +1,16 @@
 <template>
 
-
-  <div class="wrapper">
-    <h1>Animal for loop right here</h1>
-    <div class="animals">
-      <div class="animal" v-for="animal in animals" :key="animal.id">
-        <div class="animal-header">
-          <h1>{{animal.name}}</h1>
-          <p>{{animal.species}}</p>
-        </div>
-        <div class="image">
-          <img :src="'/images/animals/'+animal.image">
-        </div>
-        <div class="animal-profile">
-          <h2>{{animal.catchphrase}}</h2>
-          <!-- <button class="auto" @click="addToCart(animal)" >Add to Cart</button> -->
-        </div>
-      </div>
-    </div>
-  </div>
+  <AnimalList :animals="$root.$data.animals"/>
 
 
 </template>
 
 <script>
+import AnimalList from "@/components/AnimalList.vue"
 export default {
   name: "Animals",
-  props: {
-    animals: Array,
-  }
+  components: {
+    AnimalList
+  },
 };
 </script>
