25be23a42b684ca5078e3d4c32a33b3a7fcd826b
Cassidy Waldrip <cassidy.waldrip@gmail.com>
2020-03-19 10:18:11 -0600

    Menu done

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">Animals</router-link> |
-      <router-link to="/about">Store</router-link>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <router-link to="/" class="navbar-brand">Rinaldi Ranch</router-link>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/about" class="nav-link">Animals</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/about" class="nav-link">Store</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
     <router-view />
     <div id="footer"></div>
   </div>
