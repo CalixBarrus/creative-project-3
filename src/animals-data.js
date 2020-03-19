71955e3874ff5ab2ca39f364e87ca606c4677b7d
Cassidy Waldrip <cassidy.waldrip@gmail.com>
2020-03-19 10:31:39 -0600

    added images

--- src/animals-data.js
+++ src/animals-data.js
@@ -1,5 +1,17 @@
-let animals = [{
-  name: "Bessie"
-}, ]
+let animals = [
+  
+  {
+  species: "cow",
+  name: "Bessie",
+  "fav-activity": "Eating grass",
+  weight: "10 furkins",
+  height: "5 feet",
+  "fav-food": "grass",
+  catchphrase: "\"I'm fat and I'm happy!\"",
+  image: "cow.png"
+  }, 
+
+
+]
 
 export default animals;
--- src/products-data.js
+++ src/products-data.js
@@ -1,6 +1,10 @@
-let products = [{
-  name: "Eggs",
-  price: 3.50
-}]
+let products = [
+  {
+    name: "Eggs",
+    price: 3.50,
+    stock: "30 cartons",
+    image: "eggs.png"
+  }
+]
 
 export default products;
