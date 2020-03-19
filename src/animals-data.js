e52d2b9b8ce1841497fb0d60ec994b467e597df4
Calix Barrus <calix.barrus@gmail.com>
2020-03-19 10:06:05 -0700

    filled out animals

--- src/animals-data.js
+++ src/animals-data.js
@@ -1,16 +1,92 @@
 let animals = [
-  
-  {
-  species: "cow",
-  name: "Bessie",
-  "fav-activity": "Eating grass",
-  weight: "10 furkins",
-  height: "5 feet",
-  "fav-food": "grass",
-  catchphrase: "\"I'm fat and I'm happy!\"",
-  image: "cow.png"
-  }, 
 
+  {
+    species: "cow",
+    name: "Bessie",
+    "fav-activity": "Eating grass",
+    weight: "10 furkins",
+    height: "5 feet",
+    "fav-food": "grass",
+    catchphrase: "\"I'm fat and I'm happy!\"",
+    image: "cow.jpeg"
+  },
+
+  {
+    species: "chicken",
+    name: "Rocky",
+    "fav-activity": "Cross-country",
+    weight: "1kg",
+    height: "7 attoparsecs",
+    "fav-food": "Beef pot pie",
+    catchphrase: "I don't want to be a pie, I don't like gravy!",
+    image: "chicken.png"
+  },
+
+  {
+    species: "sheep",
+    name: "Shawn",
+    "fav-activity": "Exploring the moon",
+    weight: "Bigger than a bread box",
+    height: "20 hand-spans",
+    "fav-food": "Viking",
+    catchphrase: "The mark of a true leader is being a good follower",
+    image: "sheep.jpg"
+  },
+
+  {
+    species: "horse",
+    name: "Shadowfax",
+    "fav-activity": "Killing orcs",
+    weight: "0.5 tons",
+    height: "About \"this\" big",
+    "fav-food": "Apples",
+    catchphrase: "He sure didn't pass...",
+    image: "horse.png"
+  },
+
+  {
+    species: "pig",
+    name: "Babe",
+    "fav-activity": "Baseball",
+    weight: "2 small pigs",
+    height: "3.5 feet",
+    "fav-food": "bacon",
+    catchphrase: "Always take quantity over quality.",
+    image: "pig.jpeg"
+  },
+
+  {
+    species: "dog",
+    name: "Zelda",
+    "fav-activity": "Being cute",
+    weight: "60 lbs",
+    height: "2.5 feet",
+    "fav-food": "treat!?",
+    catchphrase: "I'm a pirahna",
+    image: "dog.jpg"
+  },
+
+  {
+    species: "cat",
+    name: "Pixie",
+    "fav-activity": "Also being cute",
+    weight: "About a loaf of bread",
+    height: "lap-size",
+    "fav-food": "Chipmunks",
+    catchphrase: "I hate Mondays...",
+    image: "cat.jpeg"
+  },
+
+  {
+    species: "goat",
+    name: "Tailor swyft",
+    "fav-activity": "Listening to Spotify",
+    weight: "80 lbs",
+    height: "1.5 ft",
+    "fav-food": "Furniture",
+    catchphrase: "We are never getting back together.",
+    image: "goat.png"
+  },
 
 ]
 
